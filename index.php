13abab39ed638b089b4879a64197d8e4622f2e48
RDiasMartins <renan2204@outlook.com>
2020-02-19 20:34:54 -0300

    Ex2020.02.19

--- index.php
+++ index.php
@@ -1,31 +1,33 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8" />
-  <title> Meus Exercícios - Renan </title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" href="bootstrap.min.css" />
-  <link rel="stylesheet" type="text/css" href="estilo.min.css" />
-  <link href = "https://fonts.googleapis.com/icon?family=Material+Icons" rel = "stylesheet"/>
+    <meta charset="utf-8" />
+    <title> Meus Exercícios - Renan </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="estilo.min.css" />
+    <link href = "https://fonts.googleapis.com/icon?family=Material+Icons" rel = "stylesheet"/>
 </head>
 <body>
-  <div class="container-fluid">
-    <?php
-    include "menu.inc";
-    ?>
-    <br/>
+    <div class="container-fluid">
+        <?php
+        include "menu.inc";
+        ?>
+        <br/>
 
-    <table class="table table-light table-striped table-hover">
-      <thead class="thead-dark">
-        <tr>
-          <th>Data</th>
-          <th>Link</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>19-02-2020</td>
-          <td> <a href="2020.02.19/Barbearia/index.php"> Barbearia </a> </td>
-        </tr>
-      </tbody>
-</html>
+        <table class="table table-light table-striped table-hover">
+            <thead class="thead-dark">
+                <tr>
+                    <th>#</th>
+                    <th>Data</th>
+                    <th>Link</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>19-02-2020</td>
+                    <td> <a href="2020.02.19/Barbearia/index.php"> Barbearia </a> </td>
+                </tr>
+            </tbody>
+            </html>
