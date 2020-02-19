14427ab9aae1ee5ef7b694c68c5366a1ed199e5b
RDiasMartins <renan2204@outlook.com>
2020-02-19 16:49:12 -0300

    Ex2020.02.19

--- index.php
+++ index.php
@@ -1,7 +1,31 @@
-<h2> Links para meus exercícios - Renan Martins </h2>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <title> Meus Exercícios - Renan </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="bootstrap.min.css" />
+  <link rel="stylesheet" type="text/css" href="estilo.min.css" />
+  <link href = "https://fonts.googleapis.com/icon?family=Material+Icons" rel = "stylesheet"/>
+</head>
+<body>
+  <div class="container-fluid">
+    <?php
+    include "menu.inc";
+    ?>
+    <br/>
 
-<hr>
-
-<br/>
-
-<a href="2020.02.19/Barbearia/index.php"> Barbearia </a>
+    <table class="table table-light" width="50%">
+      <thead class="table-dark">
+        <tr>
+          <th>Data</th>
+          <th>Link</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>19-02-2020</td>
+          <td> <a href="2020.02.19/Barbearia/index.php"> Barbearia </a> </td>
+        </tr>
+      </tbody>
+</html>
