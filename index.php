3ef9de19aaedc558fc46d2ef649c0eddd5f2eec8
RDiasMartins <renan2204@outlook.com>
2020-03-04 17:27:54 -0300

    Exercicios WEB

--- index.php
+++ index.php
@@ -1,39 +1,40 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8" />
-    <title> Meus Exercícios - Renan </title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="bootstrap.min.css" />
-    <link rel="stylesheet" type="text/css" href="estilo.min.css" />
-    <link href = "https://fonts.googleapis.com/icon?family=Material+Icons" rel = "stylesheet"/>
-</head>
-<body>
-    <div class="container-fluid">
-        <?php
-        include "menu.inc";
-        ?>
-        <br/>
+    <head>
+        <meta charset="utf-8" />
+        <title> Meus Exercícios - Renan </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="bootstrap.min.css" />
+        <link rel="stylesheet" type="text/css" href="estilo.min.css" />
+        <link href = "https://fonts.googleapis.com/icon?family=Material+Icons" rel = "stylesheet"/>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <?php
+                include "menu.inc";
+            ?>
+            <br/>
 
-        <table class="table table-light table-striped table-hover">
-            <thead class="thead-dark">
-                <tr>
-                    <th>#</th>
-                    <th>Data</th>
-                    <th>Exercício</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>19-02-2020</td>
-                    <td> <a href="2020.02.19/Barbearia/index.php"> Barbearia </a> </td>
-                </tr>
-				<tr>
-					<td>2</td>
-					<td>04-03-2020</td>
-					<td> <a href="2020.03.04/Exercicio1.html"> Validação JS </a> </td>
-				</tr>
-            </tbody>
-</body>
+            <table class="table table-light table-striped table-hover">
+                <thead class="thead-dark">
+                    <tr>
+                        <th>#</th>
+                        <th>Data</th>
+                        <th>Exercício</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>19-02-2020</td>
+                        <td> <a href="2020.02.19/Barbearia/index.php"> Barbearia </a> </td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>04-03-2020</td>
+                        <td> <a href="2020.03.04/Exercicio1.html"> Validação JS </a> </td>
+                    </tr>
+                </tbody>
+            </table>
+    </body>
 </html>
